3e59b08a7fa06fa654cd3347e940180e42288a3b
luke-zhuo <119552253+luke-zhuo@users.noreply.github.com>
2023-10-31 11:51:43 +0800

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Button = System.Windows.Forms.Button;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        //int display = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (textBox_result.Text == "0")
+                textBox_result.Clear();
+            Button button = (Button)sender;
+            textBox_result.Text = textBox_result.Text + button.Text;
+
+
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+        }
+    }
+}
